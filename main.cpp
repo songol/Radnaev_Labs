e9b4f85c831187318b45313205b5d379f5b5ada8
Tumen <t.radnaev@g.nsu.ru>
2018-12-22 01:21:36 +0700

    Iteration #2

--- main.cpp
+++ main.cpp
@@ -8,12 +8,17 @@
 #include <iomanip>
 
 int main(int argc, char *argv[]) {
+	if (argc != 3) {
+        std::cout << "Input error (must be 3 args)"
+                << std::endl;
+        return 1;
+    }
     const char *s1 = argv[1];
     const char *s2 = argv[2];
     std::string word;
     std::ifstream input;
     std::map<std::string, int32_t> wordCount;                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                   
-    char let;
+    char let = 'A';
     int32_t sum = 0;
     input.open(s1);
     while (!input.eof()) {
@@ -39,7 +44,14 @@ int main(int argc, char *argv[]) {
     });
     for (int32_t i = 0; i < res.size(); ++i) {
         double freq = 100 * (double)(res[i].second) / (double)sum;
-        output << res[i].first <<" : "<< res[i].second << " : "  << std::fixed << std::setprecision(2) << freq << "%" <<  std::endl;
+        output << res[i].first << " : " 
+        	<< res[i].second 
+        	<< " : "  
+        	<< std::fixed 
+        	<< std::setprecision(2) 
+        	<< freq 
+        	<< "%" 
+        	<<  std::endl;
     }
     output.close();
     return 0;
