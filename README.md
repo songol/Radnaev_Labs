75171853bc02fe05f6969a8a1c9820b5a5d443c6
Tumen <t.radnaev@g.nsu.ru>
2018-12-23 22:39:55 +0700

    Compile time

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Radnaev_Labs
+Realized integration of x^2+x+1 from 0 to 2 in compile time with gtest
---
+++ main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <cmath>
+#include <math.h>
+#include <gtest/gtest.h>
+
+constexpr double f1(double x) {
+    return x * x + x + 1;
+}
+
+TEST(f, _1times1) {
+    EXPECT_EQ(2, 2 * 2 + 2 + 1);
+}
+
+constexpr double integration(double left, 
+                    double right,
+                    int32_t steps,
+                    double (*func)(double )) {
+    double res = 0;
+    double h = (right - left) / steps;
+    for (int32_t i = 0; i <= steps; ++i) {
+        res += func(left + h*i) * h;
+    }
+    return res;
+}
+
+TEST(integration, integration) {
+    EXPECT_LT(20 / 3, integration(0, 2, 1000, f1));
+}
+
+int main(int argc, char** argv) {
+    constexpr auto intergral = integration(0, 2, 1000, f1);
+    std::cout << "integration = " << intergral << std::endl;
+    return 0;
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
